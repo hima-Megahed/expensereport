83249dcfc3ae1cc9982e4970863086fef48f58fd
Ibrahim Megahed <ibrahim__hasan@outlook.com>
2025-12-30 23:15:52 +0300

    format tests
    Signed-off-by: Ibrahim Megahed <ibrahim__hasan@outlook.com>

--- expensereport-csharp/expensereport-ut/UnitTest1.cs
+++ expensereport-csharp/expensereport-ut/UnitTest1.cs
@@ -3,76 +3,75 @@ using System.IO;
 using expensereport_csharp;
 using NUnit.Framework;
 
-namespace Tests
+namespace Tests;
+
+public class Tests
 {
-    public class Tests
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+    }
 
-        [Test]
-        public void AddingExpenseShouldAppearInReport()
-        {
-            using var sw = new StringWriter();
-            Console.SetOut(sw);
+    [Test]
+    public void AddingExpenseShouldAppearInReport()
+    {
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
 
-            // Act
-            var expenseReport = new ExpenseReport();
-            expenseReport.PrintReport([
-                new Expense { amount = 100, type = ExpenseType.DINNER },
-                new Expense { amount = 200, type = ExpenseType.BREAKFAST },
-                new Expense { amount = 300, type = ExpenseType.CAR_RENTAL }
-            ]);
+        // Act
+        var expenseReport = new ExpenseReport();
+        expenseReport.PrintReport([
+            new Expense { amount = 100, type = ExpenseType.DINNER },
+            new Expense { amount = 200, type = ExpenseType.BREAKFAST },
+            new Expense { amount = 300, type = ExpenseType.CAR_RENTAL }
+        ]);
 
-            // Assert
-            var output = sw.ToString();
-            Assert.IsTrue(output.Contains("Dinner\t100"));
-            Assert.IsTrue(output.Contains("Breakfast\t200"));
-            Assert.IsTrue(output.Contains("Car Rental\t300"));
-        }
+        // Assert
+        var output = sw.ToString();
+        Assert.IsTrue(output.Contains("Dinner\t100"));
+        Assert.IsTrue(output.Contains("Breakfast\t200"));
+        Assert.IsTrue(output.Contains("Car Rental\t300"));
+    }
 
-        [Test]
-        public void DinnerExpenseMoreThan5000ShouldReturnXInReport()
-        {
-            using var sw = new StringWriter();
-            Console.SetOut(sw);
+    [Test]
+    public void DinnerExpenseMoreThan5000ShouldReturnXInReport()
+    {
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
 
-            // Act
-            var expenseReport = new ExpenseReport();
-            expenseReport.PrintReport([
-                new Expense { amount = 6000, type = ExpenseType.DINNER },
-                new Expense { amount = 200, type = ExpenseType.BREAKFAST },
-                new Expense { amount = 300, type = ExpenseType.CAR_RENTAL }
-            ]);
+        // Act
+        var expenseReport = new ExpenseReport();
+        expenseReport.PrintReport([
+            new Expense { amount = 6000, type = ExpenseType.DINNER },
+            new Expense { amount = 200, type = ExpenseType.BREAKFAST },
+            new Expense { amount = 300, type = ExpenseType.CAR_RENTAL }
+        ]);
 
-            // Assert
-            var output = sw.ToString();
-            Assert.IsTrue(output.Contains("Dinner\t6000\tX"));
-            Assert.IsTrue(output.Contains("Breakfast\t200"));
-            Assert.IsTrue(output.Contains("Car Rental\t300"));
-        }
+        // Assert
+        var output = sw.ToString();
+        Assert.IsTrue(output.Contains("Dinner\t6000\tX"));
+        Assert.IsTrue(output.Contains("Breakfast\t200"));
+        Assert.IsTrue(output.Contains("Car Rental\t300"));
+    }
 
-        [Test]
-        public void BreakfastExpenseMoreThan1000ShouldReturnXInReport()
-        {
-            using var sw = new StringWriter();
-            Console.SetOut(sw);
+    [Test]
+    public void BreakfastExpenseMoreThan1000ShouldReturnXInReport()
+    {
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
 
-            // Act
-            var expenseReport = new ExpenseReport();
-            expenseReport.PrintReport([
-                new Expense { amount = 6000, type = ExpenseType.DINNER },
-                new Expense { amount = 10000, type = ExpenseType.BREAKFAST },
-                new Expense { amount = 300, type = ExpenseType.CAR_RENTAL }
-            ]);
+        // Act
+        var expenseReport = new ExpenseReport();
+        expenseReport.PrintReport([
+            new Expense { amount = 6000, type = ExpenseType.DINNER },
+            new Expense { amount = 10000, type = ExpenseType.BREAKFAST },
+            new Expense { amount = 300, type = ExpenseType.CAR_RENTAL }
+        ]);
 
-            // Assert
-            var output = sw.ToString();
-            Assert.IsTrue(output.Contains("Dinner\t6000\tX"));
-            Assert.IsTrue(output.Contains("Breakfast\t10000\tX"));
-            Assert.IsTrue(output.Contains("Car Rental\t300"));
-        }
+        // Assert
+        var output = sw.ToString();
+        Assert.IsTrue(output.Contains("Dinner\t6000\tX"));
+        Assert.IsTrue(output.Contains("Breakfast\t10000\tX"));
+        Assert.IsTrue(output.Contains("Car Rental\t300"));
     }
 }
