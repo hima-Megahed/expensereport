fe974822fda6f9b35f51063146763715be5a47ca
Ibrahim Megahed <ibrahim__hasan@outlook.com>
2025-12-30 22:37:32 +0300

    add characterization test

--- expensereport-csharp/expensereport-ut/UnitTest1.cs
+++ expensereport-csharp/expensereport-ut/UnitTest1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using expensereport_csharp;
 using NUnit.Framework;
 
 namespace Tests
@@ -10,9 +14,25 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void AddingExpenseDinnerShouldReturnDinnerExpense()
         {
-            Assert.Pass();
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                
+                // Act
+                var expenseReport = new ExpenseReport();
+                expenseReport.PrintReport(new List<Expense>
+                {
+                    new Expense {amount = 100, type = ExpenseType.DINNER},
+                    // new Expense {amount = 200, type = ExpenseType.BREAKFAST},
+                    // new Expense {amount = 300, type = ExpenseType.CAR_RENTAL}
+                });
+                
+                // Assert
+                var output = sw.ToString();
+                Assert.Equals("",  output);
+            };
         }
     }
 }
